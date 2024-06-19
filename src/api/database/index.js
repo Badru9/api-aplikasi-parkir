e7fd0e2708b378f460e74e0b5513446f5a5a1318
Badru9 <mohbadrujaman9@gmail.com>
2024-06-19 11:27:08 +0700

    feat[all] : update

--- src/api/database/index.js
+++ src/api/database/index.js
@@ -11,22 +11,6 @@
 //   password: process.env.PASSWORD,
 //   database: process.env.DATABASE,
 // });
-=======
-const database = mySQL.createPool({
-  // host: "localhost",
-  // user: "badru",
-  // password: "badru123",
-  // database: "aplikasi_parkir",
-  // host: process.env.HOST,
-  // user: process.env.USER,
-  // password: process.env.PASSWORD,
-  // database: process.env.DATABASE,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "aplikasi_parkir",
-});
->>>>>>> da1033935c66293bec5ed2112efc99cb43d6eaea
 
 // module.exports = database;
 
